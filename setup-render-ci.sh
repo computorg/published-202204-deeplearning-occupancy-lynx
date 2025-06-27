2703b665c13870726c626caf0988f94aba964699
Julien Chiquet <julien.chiquet@inrae.fr>
2025-06-27 09:41:04 +0200

    progress, but no  trying to add ssl support

--- setup-render-ci.sh
+++ setup-render-ci.sh
@@ -1,1 +1,5 @@
-sudo apt install -y libssl-dev libabsl-dev libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite3-dev libglpk40
+sudo apt install -y libssl-dev libabsl-dev libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite3-dev libglpk40 libcurl4-openssl-dev
+
+
+
+
