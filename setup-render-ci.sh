101501d3a44dbe2a38108ff825608e444d9beaf5
Julien Chiquet <julien.chiquet@inrae.fr>
2025-06-27 10:59:29 +0200

    another render dep

--- setup-render-ci.sh
+++ setup-render-ci.sh
@@ -1,4 +1,4 @@
-sudo apt install -y libssl-dev libabsl-dev libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite3-dev libglpk40 libcurl4-openssl-dev
+sudo apt install -y libssl-dev libabsl-dev libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite3-dev libglpk40 libcurl4-openssl-dev libffi-dev
 
 
 
