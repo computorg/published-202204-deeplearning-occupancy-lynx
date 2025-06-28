7c7265bad3076b2f4cef06be959ab09a0381fc39
Julien Chiquet <julien.chiquet@inrae.fr>
2025-06-28 10:14:05 +0200

    certificate for urlilb

--- setup-render-ci.sh
+++ setup-render-ci.sh
@@ -1,4 +1,19 @@
-sudo apt install -y libssl-dev libabsl-dev libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite3-dev libglpk40 libcurl4-openssl-dev libffi-dev
+sudo apt-get update
+sudo apt-get install -y \
+    ca-certificates \
+    openssl \
+    libssl-dev \
+    libcurl4-openssl-dev \
+    curl \
+    wget \
+    libabsl-dev \
+    libudunits2-dev \
+    libgdal-dev \
+    libgeos-dev \
+    libproj-dev \
+    libsqlite3-dev \
+    libglpk40
+
 
 
 
