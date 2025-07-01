b7dec2cd6e5bc962387a4690c89250e57ad68b10
Julien Chiquet <jchiquet@users.noreply.github.com>
2025-07-01 14:44:13 +0200

    New workflow (#6)
    * lots of tricks, it is now working

---
+++ setup-render-ci.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get install -y \
+    ca-certificates \
+    libssl-dev \
+    curl \
+    libcurl4-openssl-dev \
+    wget \
+    libabsl-dev \
+    libudunits2-dev \
+    libgdal-dev \
+    libgeos-dev \
+    libproj-dev \
+    libsqlite3-dev \
+    libglpk40
+sudo update-ca-certificates --fresh
