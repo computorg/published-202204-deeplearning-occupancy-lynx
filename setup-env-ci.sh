03b380d4d4a220484a3cc0dcef77b2100f19ced7
Julien Chiquet <julien.chiquet@inrae.fr>
2025-07-01 06:59:04 +0200

    openssl both in env and rendering

--- setup-env-ci.sh
+++ setup-env-ci.sh
@@ -1,1 +1,24 @@
-sudo apt install -y libcurl4-openssl-dev libglpk-dev libpng-dev libx11-dev pandoc jags libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libssl-dev libabsl-dev cmake libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite3-dev
+sudo apt-get update
+sudo apt-get install -y \
+    libcurl4-openssl-dev \
+    libglpk-dev \
+    libpng-dev \
+    libx11-dev \
+    pandoc \
+    jags \
+    libfontconfig1-dev \
+    libharfbuzz-dev \
+    libfribidi-dev \
+    libfreetype6-dev \
+    libpng-dev \
+    libtiff5-dev \
+    libjpeg-dev \
+    libssl-dev \
+    libabsl-dev \
+    cmake \
+    libudunits2-dev \
+    libgdal-dev \
+    libgeos-dev \
+    libproj-dev \
+    libsqlite3-dev \
+    openssl
